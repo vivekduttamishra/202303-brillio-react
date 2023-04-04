d149d15c0620d8d955dfd0131b4167fbabcd668c
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2023-04-04 11:03:31 +0530

    day#10 update#2

--- books/src/app.js
+++ books/src/app.js
@@ -3,15 +3,25 @@ import Footer from './components/footer.components';
 import BookListScreen from './screens/book-list-screen.component';
 import BookDetailsScreen from "./screens/book-details-screen";
 import BookAddScreen from "./screens/book-add-screen";
+import * as Consts from './consts';
+import { useState } from "react";
 
 const App=(props)=>{
+
+    //var screen=Consts.SCREEN_BOOK_LIST;
+
+    var [screen, navigate] = useState(Consts.SCREEN_BOOK_LIST);
+
     return (
         <div>
-            <NavigationBar title='World Wide Books'/>
+            <NavigationBar title='World Wide Books' onNavigate={navigate} />
             <div className='screen-container'>
-                <BookAddScreen/>
-                <BookListScreen/>
-                <BookDetailsScreen/>
+
+                {screen===Consts.SCREEN_BOOK_ADD && <BookAddScreen/>}
+
+                {screen===Consts.SCREEN_BOOK_LIST && <BookListScreen/>}
+
+                {screen===Consts.SCREEN_BOOK_DETAILS && <BookDetailsScreen/>}
             </div>
             <Footer/>
         </div>
